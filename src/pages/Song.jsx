e155b3ad67d78716b72cdb126531010ad17d5348
evielliot <zap01462@zapopan.tecmm.edu.mx>
2023-06-22 22:26:32 -0600

    Buttons

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -5,12 +5,14 @@ import { firestore } from "../firebase";
 import { inputCleaner } from "../helpers/inputCleaner";
 import YoutubeEmbed from "./YoutubeEmbed";
 import Youtube, { youtube } from "../helpers/youtube";
-import { LuHeadphones } from 'react-icons/lu';
+import { LuHeadphones, LuArrowLeft, LuArrowRight, LuAtom } from 'react-icons/lu';
 
 function Song() {
   const { song } = useParams();
   const sanitized = inputCleaner(song);
   const [songData, setSongData] = useState(null);
+  const [songs, setSongs] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const fetchSongData = async () => {
@@ -38,12 +40,53 @@ function Song() {
     fetchSongData();
   }, [sanitized]);
 
+  useEffect(() => {
+    const fetchSongs = async () => {
+      try {
+        // Consultar todas las canciones
+        const songsRef = collection(firestore, "songs");
+        const querySnapshot = await getDocs(songsRef);
+
+        if (!querySnapshot.empty) {
+          // Almacenar todas las canciones en el estado
+          const songsData = querySnapshot.docs.map((doc) => doc.data());
+          setSongs(songsData);
+        } else {
+          // No se encontraron canciones
+          console.log("No se encontraron canciones");
+          setSongs([]);
+        }
+      } catch (error) {
+        console.error("Error al obtener las canciones:", error);
+      }
+    };
+
+    fetchSongs();
+  }, []);
+
   const handleGoHome = () => {
     window.location.href = "/";
   };
 
   const handleGoToSongs = () => {
     window.location.href = "/songs";
+  };
+
+  const handlePreviousSong = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+
+  const handleRandomSong = () => {
+    const randomIndex = Math.floor(Math.random() * songs.length);
+    setCurrentIndex(randomIndex);
+  };
+
+  const handleNextSong = () => {
+    if (currentIndex < songs.length - 1) {
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+    }
   };
 
   if (!songData) {
@@ -55,16 +98,33 @@ function Song() {
 
   return (
     <div className="background">
-        <div className="container container-song">
-        <h1 className="text-center titleword">{<LuHeadphones />} {songName} {<LuHeadphones />}</h1>
+      <div className="container container-song">
+        <h1 className="text-center titleword">
+          <LuHeadphones /> {songName} <LuHeadphones />
+        </h1>
         <YoutubeEmbed embedId={youtube(songLink)} />
         <p className="dedication">{dedication}</p>
         <hr />
         <div className="d-grid gap-2">
-            <button className="btn btn-pinky" onClick={handleGoHome}>Ir a la página de inicio</button>
-            <button className="btn btn-pinky" onClick={handleGoToSongs}>Ver lista de canciones</button>
+          <button className="btn btn-pinky" onClick={handleGoHome}>
+            Ir a la página de inicio
+          </button>
+          <button className="btn btn-pinky" onClick={handleGoToSongs}>
+            Ver lista de canciones
+          </button>
         </div>
+        <div className="d-flex justify-content-center mt-3">
+          <button
+            className="btn btn-primary mx-2 btn-pinky"
+            onClick={() => {
+              const randomIndex = Math.floor(Math.random() * songs.length);
+              window.location.href = `/song/${songs[randomIndex].slug}`;
+            }}
+          >
+            <LuAtom /> Canción random
+          </button>
         </div>
+      </div>
     </div>
   );
 }
